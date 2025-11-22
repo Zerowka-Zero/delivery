47968ef11f60345c0e72186b3ceae3ca4e9f2463
BelikAV <belikv199027@gmail.com>
2025-11-22 14:21:09 +0300

    added an entity

--- src/main/java/ru/artem/delivery/model/User.java
+++ src/main/java/ru/artem/delivery/model/User.java
@@ -1,10 +1,15 @@
 package ru.artem.delivery.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 
 @Entity
 public class User {
+    @id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     private String name;
     private String surname;
     private String email;
