e941d3c9b59bc67aec90f64c6ef1d832ada8c680
BelikAV <belikv199027@gmail.com>
2025-11-26 23:14:08 +0300

    corrected the comments PR

--- src/main/java/ru/artem/delivery/model/User.java
+++ src/main/java/ru/artem/delivery/model/User.java
@@ -3,10 +3,13 @@ package ru.artem.delivery.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
 
 @Entity
+@Data
 public class User {
-    @id
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
@@ -14,57 +17,6 @@ public class User {
     private String surname;
     private String email;
     private String phone;
-
-    public User() {}
-
-    public User(int id, String name, String surname, String email, String phone) {
-        this.id = id;
-        this.name = name;
-        this.surname = surname;
-        this.email = email;
-        this.phone = phone;
-
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
 
     @Override
     public String toString() {
